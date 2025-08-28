b7692a791032f2359418503065421a37806a51c6
Miked0331 <mikedragun@gmail.com>
2025-08-28 12:37:37 -0400

    Test deploy change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,6 @@ export default function Home() {
   return (
     
     <div className="text-center mt-20 text-black">
-    <h1>Hello Arena v2</h1>
-
       <h1 className="text-4xl font-bold mb-4">Welcome to the Arena</h1>
       <p className="mb-6 text-lg text-gray-500">
         Compete. Dominate. Rise in the ranks.
